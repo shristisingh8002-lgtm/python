ac8c575b0d172a9bc9d78be5f5199892db92be2b
shristisingh8002-lgtm <shristisingh8002@gmail.com>
2026-07-18 18:44:45 +0530

    Add files via upload

---
+++ lec8.py
@@ -0,0 +1,8 @@
+a=50 
+b=3 
+
+print ("the value of " , a, " + " , b, "is: ", a + b)
+print ("the value of " , a, " - " , b, "is: ", a - b)
+print ("the value of " , a, " * " , b, "is: ", a * b)
+print ("the value of " , a, " / " , b, "is: ", a / b)
+print ("the value of " , a, " % " , b, "is: ", a % b)
