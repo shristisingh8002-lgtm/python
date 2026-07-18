9725a6e4d4b6b9a729fa0b9783b2b2a643aeb93d
shristisingh8002-lgtm <shristisingh8002@gmail.com>
2026-07-18 18:44:17 +0530

    Add files via upload

---
+++ lec6.py
@@ -0,0 +1,15 @@
+a= complex(8,2)
+print(a)
+ 
+ 
+c="harry"
+d='none'
+print(a)
+a1=9
+print(a + a1)
+print("the type of a is",type(a))
+print("the type of c is",type(c))
+
+
+list1=[8,  2,3,[-4,5],["apple","banana"]]
+print (list1)
