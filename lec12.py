76415c05f5e2e81495de6a1405c531ba973eb9d0
shristisingh8002-lgtm <shristisingh8002@gmail.com>
2026-07-28 19:02:15 +0530

    Add files via upload

---
+++ lec12.py
@@ -0,0 +1,15 @@
+fruit = "mango"
+mangolen = len(fruit)
+print( mangolen )
+#print( fruit[0:4] ) including 0 but not 4 
+#print( fruit[1:4] )
+#print( fruit[:5] )
+#print (fruit[0:-3])
+#print(fruit[0: len(fruit)-3])
+print(fruit[-1: len(fruit)-3])
+print(fruit[-3:-1])
+
+#quick quiz
+#nm = "harry"
+#print(nm[-4:-2])
+# code with harry 
