fe90185eced7db993415e1b0123ea63636b6390f
shristisingh8002-lgtm <shristisingh8002@gmail.com>
2026-07-28 19:03:38 +0530

    Add files via upload

---
+++ lec16.py
@@ -0,0 +1,16 @@
+x = int(input("enter the value of x "))
+# x is the variable of match 
+ # match x:
+ # is x is 0:
+ # cas 0: print ("x is 0")
+print("x is 0")
+#case with tf-condition
+# case4:
+print("case is 4")
+
+# case_ if x!=90
+print (x,"is not 90 ") 
+# case_!=80:
+print("x,is not 80")
+# case_:
+print("x")
