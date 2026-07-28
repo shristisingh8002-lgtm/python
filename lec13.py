0cc0f09db7ea911b2c772d04594ed4237eda2b6c
shristisingh8002-lgtm <shristisingh8002@gmail.com>
2026-07-28 19:02:45 +0530

    Add files via upload

---
+++ lec13.py
@@ -0,0 +1,50 @@
+# strings are immutable 
+a = "harrry!!!!!!!!!!!! harry"
+print(len(a))
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("harrry", "john"))
+print(a.split(" "))
+blogheaading = "introduction tO JS "
+print(blogheaading)
+print(blogheaading.capitalize())
+
+str1 = "welcome to console"
+print(len(str1))
+print(len(str1.center(50,)))
+print(a.count("harrry"))
+
+str1 = "welcome to console !!!"
+print(str1.endswith("!!!"))
+
+str1 = "welcome to console !!!"
+print(str1.startswith("to", 4 , 10))
+str1 = "hes's name is dan, he is an honest man"
+print(str1.find("ishh"))
+str1 = "welcome to console"
+print(str1 . isalnum())
+str1 = "welcome0"
+print(str1 . isalpha())
+
+str1 = "hello world"
+print(str1 . islower())
+str1 = "we wish you merry christmas\n"
+print(str1 . isprintable())
+str1 = "         "      # using spacebar
+print(str1 . isspace())
+str2 = "       "      # using tab
+print(str2 . isspace())
+
+str1 = "hello heaith organization"
+print(str1 . istitle())
+
+str2 = "to kill a mokingbird"
+
+str1 = "python is a interpreted language"
+
+str1 = "python is a interpreted language"
+print(str1 . swapcase() )
+
+sr1 = "his name is dan, dan is a honest man ."
+print(str1.title( ))
