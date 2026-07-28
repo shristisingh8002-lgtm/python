70bb4cd862c4af91039bea212c34ba8220f035ec
shristisingh8002-lgtm <shristisingh8002@gmail.com>
2026-07-28 19:03:03 +0530

    Add files via upload

---
+++ lec14.py
@@ -0,0 +1,19 @@
+a = int(input("enter your age"))
+print("your age is :" , a )
+# conditional operators
+#>,<, >=, <=, == 
+print(a>10)
+print(a<18)
+print(a ==18)
+if(a>18):
+    print ("you can drive")
+    print("yes")
+else:
+    print("you cannot drive")
+    print("no")
+    print("yay!")
+    
+    num = int(input("enter the value of num : "))
+ 
+        
+        
