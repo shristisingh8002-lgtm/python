2b22cf634eabea6974d9c04fcdede70470e76b6d
shristisingh8002-lgtm <shristisingh8002@gmail.com>
2026-07-28 19:03:56 +0530

    Add files via upload

---
+++ lec17.py
@@ -0,0 +1,23 @@
+
+name = "abhishek"
+for i in name :
+      print(i)
+ #    if (i == "b") :
+       #  print ("this is something special")
+         
+color = ["red", "green" , "blue" , "yellow"] 
+for color in color:
+    print(color) 
+for i in color : 
+     print(i)
+      
+
+for k in range (5) :
+    print(k + 1)
+    
+    for k in range (1,2001):
+        print(k)
+
+
+for k in range(2,12,15): 
+    print(k)
