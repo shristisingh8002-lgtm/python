a02a4653c7243f6242fc8d113ff671db4ff80296
shristisingh8002-lgtm <shristisingh8002@gmail.com>
2026-07-28 19:01:58 +0530

    Add files via upload

---
+++ lec11.py
@@ -0,0 +1,20 @@
+name = "harry"
+friend = "rohan"
+anotherfriend = "lavish"
+apple = '''he said
+hi harry
+hey i am good
+"i want to eat an apple"'''
+
+print("hello " + name)
+# print(apple)
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+#print(name[5]) # throws an error
+print("lets use a for loop\n")
+for character in apple:
+    print(character)
