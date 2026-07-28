80a76178078d95d1356ff5d3801fb2a5742c64b3
shristisingh8002-lgtm <shristisingh8002@gmail.com>
2026-07-28 19:03:21 +0530

    Add files via upload

---
+++ lec15.py
@@ -0,0 +1,10 @@
+import time  
+timestamp = time.strf('%H : %M : %S ')
+print(timestamp)
+timestamp = int(time.strftime ('%H') )
+print(timestamp)
+timestamp = int(time.strftime ('%M') )
+print (timestamp)
+timestamp = int(time.strf ('%S') )
+print = (timestamp)
+
